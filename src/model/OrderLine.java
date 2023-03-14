bf33e07cf133a6d71426e581a1a449d1a01d4aae
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-03-14 13:11:30 +0100

    Added orderLine & product

--- src/model/OrderLine.java
+++ src/model/OrderLine.java
@@ -1,5 +1,42 @@
 package model;
 
 public class OrderLine {
+	private Product product;
+	private int quantity;
+	
+	public OrderLine(Product product, int quantity) {
+		this.product = product;
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
 
 }
