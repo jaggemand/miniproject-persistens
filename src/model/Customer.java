ffbcc57e78e07834cd731cb9e1dd18db2d35821e
NikolaiCS <113992103+NikolaiCS@users.noreply.github.com>
2023-03-14 12:57:21 +0100

    added Customer

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,106 @@
 package model;
 
 public class Customer {
+	private String name;
+	private String email;
+	private String address;
+	private String zipcode;
+	private String city;
+	private String phoneNumber;
+	
+	public Customer(String name, String email, String address, String zipcode, String city, String phoneNumber) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.zipcode = zipcode;
+		this.city = city;
+		this.phoneNumber = phoneNumber;
+	}
 
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the zipcode
+	 */
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	/**
+	 * @param zipcode the zipcode to set
+	 */
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
