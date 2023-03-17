4952733e08ef776cfe45d0e820d0138fb587771c
esserdk88 <rasmus.lyngberg@gmail.com>
2023-03-17 11:06:47 +0100

    Update OrderDBIF.java

--- src/dal/OrderDBIF.java
+++ src/dal/OrderDBIF.java
@@ -1,7 +1,19 @@
 package dal;
 
 import model.Order;
+/**
+Last updated: 17-03-2023
 
+- Documentation and comments added
+*/
+/**
+Interface for OrderDB. Defines method for adding a new order to the database.
+*/
 public interface OrderDBIF {
+	/**
+	Adds a new order to the database.
+	@param o The Order object to be added.
+	@return The Order object with updated information, including the generated order number.
+	*/
 	public Order addNewOrder(Order o);
 }
