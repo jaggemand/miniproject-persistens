e739b0f14284274caa71cce4a62c38314bda7756
esserdk88 <rasmus.lyngberg@gmail.com>
2023-03-17 12:21:17 +0100

    Update ProductTest.java

--- src/test/ProductTest.java
+++ src/test/ProductTest.java
@@ -13,6 +13,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * The ProductTest class contains unit tests for the ProductController class.
+ * It tests various methods of the ProductController class, such as creating a new product,
+ * updating product stock and location, selecting products by stock location, and removing a product.
+ */
 class ProductTest {
 
 	private ProductDB productDataBase;
@@ -24,25 +29,35 @@ class ProductTest {
 	private static int identity2;
 	private static int identity3;
 
+	/**
+	 * Sets up the test environment before running the test cases.
+	 * Creates and stores test products in the product database.
+	 * This method is run once before all the test cases are run.
+	 *
+	 * @throws Exception if an error occurs during the setup process.
+	 */
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		createTestProducts();
 	}
 
+	/**
+	 * Tears down the test environment after running the test cases.
+	 * Removes the test products from the product database.
+	 * This method is run once after all the test cases are run.
+	 *
+	 * @throws Exception if an error occurs during the teardown process.
+	 */
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 		removeTestProducts();
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
-
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	/**
+	 * Tests if the product stock amount is updated correctly.
+	 * Creates a new product controller, updates the stock of a test product,
+	 * and checks if the stock amount is updated correctly.
+	 */
 	@Test
 	void shouldUpdateProductStockTo30() {
 		productDataBase = new ProductDB();
@@ -63,6 +78,11 @@ class ProductTest {
 		assertEquals(30, checkProduct.getStockAmount());
 	}
 
+	/**
+	 * Tests if the correct number of products are returned by stock location.
+	 * Creates a new product controller, selects all products with a stock location of 50,
+	 * and checks if the number of returned products is equal to 2.
+	 */
 	@Test
 	void shouldReturn2Products() {
 		// Arrange
@@ -76,6 +96,11 @@ class ProductTest {
 		assertNotNull(list);
 	}
 
+	/**
+	 * Tests if the product location is updated correctly.
+	 * Creates a new product controller, updates the location of a test product,
+	 * and checks if the location is updated correctly.
+	 */
 	@Test
 	void shouldSetProductLocationTo2() {
 		// Arrange
@@ -91,6 +116,10 @@ class ProductTest {
 		assertTrue(success);
 	}
 
+	/**
+	 * Creates test products and stores them in the product database.
+	 * Also sets identity variables to the product numbers of the test products.
+	 */
 	static void createTestProducts() {
 		// Arrange
 		productDataBase1 = new ProductDB();
@@ -111,6 +140,9 @@ class ProductTest {
 		identity3 = product3.getProductNumber();
 	}
 
+	/**
+	Removes the test products created in the ProductTest class from the database.
+	*/
 	static void removeTestProducts() {
 		// Arrange
 		productDataBase1 = new ProductDB();
